c7c1f1686e1db00b9e6b599ec639b4d043933012
Alex Chukanov <alex.chukanov@gmail.com>
2018-01-17 11:47:37 +0000

    Update Program.cs

--- 2.Maze/Program.cs
+++ 2.Maze/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+//You are given a map of square room, some of the tiles of this room are blocked//You are standing at starting tile. 
+//You can step in four directions (top, right, bottom, left), one tile per step//Write a code that returns the minimum number
+//of steps required to get from your starting tile to the specific target tile
+////Input format:
+//First line contains a size of the room N  (3 <= N <= 80)//Next N lines contain map of the room where # means empty tile, 
+//* means blocked tile, 0 means starting tile and 1 means target tile
+////Output format:
+//A single integer equals to the minimum number of steps required or -1 in case target tile can't be reached
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
